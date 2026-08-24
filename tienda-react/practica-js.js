f90a7bc3ac7a4c6ff16f81b16fce1a23ce2715a8
Carommi <dianyyrz@gmail.com>
2026-08-24 11:42:08 -0500

    Feat: Ejercicios base de JavaScript

--- tienda-react/practica-js.js
+++ tienda-react/practica-js.js
@@ -7,7 +7,7 @@
 const resultado = calcularTotal(50000, 3);
 console.log(resultado);*/
 
-function Sumar(a, b) {
+/*function Sumar(a, b) {
     const totalSuma = a + b
     return totalSuma
 }
@@ -43,4 +43,50 @@ console.log(calcularPromedio(70, 90, 60));
 programada la función mas NO muestra ningún valor en consola.
 2. El Console.log es mucho más simple, el sirve simplemente para mostrar en consola, es decir, todo lo
 que pongamos en los paréntesis de Console.log() se va a mostrar en consola. 
-Carommi :)*/
+Carommi :)
+
+
+const aplicarIva = (precio) => {
+    return precio * 1.19;
+};
+
+console.log(aplicarIva(120000));
+*/
+
+const productos = [
+    { id: 1, nombre: 'Mouse', precio: 50000, stock: 5 },
+    { id: 2, nombre: 'Teclado', precio: 90000, stock: 0 },
+    { id: 3, nombre: 'Monitor', precio: 650000, stock: 3 },
+    { id: 4, nombre: 'MousePad', precio: 34000, stock: 15 },
+    { id: 5, nombre: 'Protector Teclado', precio: 20000, stock: 10 },
+    { id: 6, nombre: 'Mouse Ergonomico', precio: 75000, stock: 20 },
+    { id: 7, nombre: 'Silla', precio: 50000, stock: 9 },
+    { id: 8, nombre: 'Silla Ergonomica', precio: 102000, stock: 5 },
+    { id: 9, nombre: 'Portatil', precio: 3100000, stock: 4 },
+    { id: 10, nombre: 'Cable Ethernet', precio: 65000, stock: 10 }
+];
+
+const productoCopia = {
+    ...productos[4],
+    stock: 0,
+    precio: 18000
+};
+
+const estado = productoCopia.stock > 0 ? 'Disponible' : 'Agotado';
+console.log(estado);
+
+console.log(`El producto ${productoCopia.nombre} cuesta $${productoCopia.precio}`);
+
+/*
+const encontrado = productos.find(
+    producto => producto.id === 5
+);
+console.log(encontrado);
+
+const nombre = productos.map(producto => {
+    console.log(producto.nombre);
+    return producto.nombre;
+});
+*/
+
+
