8c75586da6804757c802e5c65e8f10848c1d72ec
Carommi <dianyyrz@gmail.com>
2026-08-25 11:15:39 -0500

    Feat: Agregamos eventos a ProductoCard

--- tienda-react/src/components/ProductoCard.jsx
+++ tienda-react/src/components/ProductoCard.jsx
@@ -1,31 +1,64 @@
 function ProductoCard({ producto }) {
-    const estado = producto.stock > 0 ? 'Disponible' : 'Agotado';
+
+    const {
+        nombre,
+        precio,
+        categoria,
+        stock
+    } = producto;
+
+    const estado = stock > 0
+        ? 'Disponible'
+        : 'Agotado';
+
+    const mostrarProducto = () => {
+        alert(`Seleccionaste ${nombre}`);
+    };
 
     return (
         <article className="producto-card">
+
             <div className="producto-id">
                 #{producto.id}
             </div>
 
             <div className="producto-info">
+
                 <span className="categoria">
-                    {producto.categoria}
+                    {categoria}
                 </span>
 
-                <h2>{producto.nombre}</h2>
+                <h2>{nombre}</h2>
 
                 <p className="precio">
-                    ${producto.precio.toLocaleString('es-CO')}
+                    ${precio.toLocaleString('es-CO')}
                 </p>
 
                 <p className="stock">
-                    Stock disponible: <strong>{producto.stock}</strong>
+                    Stock disponible: <strong>{stock}</strong>
                 </p>
+
             </div>
 
-            <span className={`estado ${producto.stock > 0 ? 'estado-disponible' : 'estado-agotado'}`}>
+            <span
+                className={
+                    `estado ${stock > 0
+                        ? 'estado-disponible'
+                        : 'estado-agotado'
+                    }`
+                }
+            >
                 {estado}
             </span>
+
+            <button
+                className="boton-producto"
+                onClick={mostrarProducto}
+                disabled={stock === 0}
+            >
+                {stock > 0 ? 'Ver producto' : 'Agotado'}
+            </button>
+
         </article>
     );
 }
