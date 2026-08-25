f2a2685bb02b90e6d02975261c71e573f0383c19
Carommi <dianyyrz@gmail.com>
2026-08-25 10:43:10 -0500

    Feat: Se modifico la estetica de ProductosCard y App.css para mejorar la calidad visual

--- tienda-react/src/components/ProductoCard.jsx
+++ tienda-react/src/components/ProductoCard.jsx
@@ -1,13 +1,33 @@
 function ProductoCard({ producto }) {
     const estado = producto.stock > 0 ? 'Disponible' : 'Agotado';
+
     return (
         <article className="producto-card">
-            <h2>{producto.nombre}</h2>
-            <p>Categoría: {producto.categoria}</p>
-            <p>Precio: ${producto.precio}</p>
-            <p>Stock: {producto.stock}</p>
-            <strong>{estado}</strong>
+            <div className="producto-id">
+                #{producto.id}
+            </div>
+
+            <div className="producto-info">
+                <span className="categoria">
+                    {producto.categoria}
+                </span>
+
+                <h2>{producto.nombre}</h2>
+
+                <p className="precio">
+                    ${producto.precio.toLocaleString('es-CO')}
+                </p>
+
+                <p className="stock">
+                    Stock disponible: <strong>{producto.stock}</strong>
+                </p>
+            </div>
+
+            <span className={`estado ${producto.stock > 0 ? 'estado-disponible' : 'estado-agotado'}`}>
+                {estado}
+            </span>
         </article>
     );
 }
+
 export default ProductoCard;
